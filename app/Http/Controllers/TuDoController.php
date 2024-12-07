593930a3fc5fcf9599d334ad6f336b34f3f835b7
Nguyen Huu Thang <nhthangvp96@gmail.com>
2024-12-07 14:25:11 +0700

    update-updateTu

--- app/Http/Controllers/TuDoController.php
+++ app/Http/Controllers/TuDoController.php
@@ -15,5 +15,27 @@
 
     class TuDoController extends Controller
     {
+        public function update(Request $request)
+        {
+            $data = TuDo::where('id', $request->id)->first();
+            if ($data) {
+                $data->update([
+                    'ten_san_pham'   => $request->ten_san_pham,
+                    'hinh_anh'       => $request->hinh_anh,
+                    'gia_ban'        => $request->gia_ban,
+                    'is_active'      => $request->is_active,
+                    'pin_active'     => $request->pin_active,
+                ]);
+                return response()->json([
+                    'status'    =>   true,
+                    'message'   =>   'Đã cập nhật danh sách tủ thành công!'
+                ]);
+            } else {
+                return response()->json([
+                    'status'    =>   false,
+                    'message'   =>   'Không tìm được tủ đồ để cập nhật!'
+                ]);
+            }
+        }
        
     }
